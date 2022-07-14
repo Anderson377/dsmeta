48e681eaf69b364c9caa70a3cfe5f0c352b14052
Anderson377 <dolce436@gmail.com>
2022-07-14 13:07:23 -0300

    primeiro commit semana spring react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header/Index"
+import SalesCard from "./components/SalesCard/Index"
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
